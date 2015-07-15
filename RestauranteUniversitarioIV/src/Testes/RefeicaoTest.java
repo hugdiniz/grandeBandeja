87811d2267d4ac734fb2f6691a4499af1012d395
Kesia <Kesia@192.168.25.92>
2015-07-15 11:22:03 -0300

    RedeiçãoTest

--- RestauranteUniversitarioIV/src/Testes/RefeicaoTest.java
+++ RestauranteUniversitarioIV/src/Testes/RefeicaoTest.java
@@ -1,25 +1,34 @@
-package Testes;
+package entidades;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import entidades.Refeicao;
-import entidades.value_objects.DepartamentoVO;
-import entidades.value_objects.RefeicaoVO;
-
-import entidades.enumerados.*;
 public class RefeicaoTest {
 
 	@Test
 	public void testGetInstance() {
-		
-		
-		RefeicaoVO refeicaoVO = new RefeicaoVO(Turno.valueOf("MANHA"),"descricao", "op_vegetariana");
-		assertNotNull(refeicaoVO);
-			
-		
+		fail("Not yet implemented");
 	}
 
-	
+	@Test
+	public void testRecuperarRefeicoes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdicionarRefeicao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRecuperarRefeicao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAtualizarRefeicao() {
+		fail("Not yet implemented");
+	}
+
 }
