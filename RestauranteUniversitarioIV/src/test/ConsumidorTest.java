ab4109405dfb5015e07f840605f5aba497321412
Administradora <Administradora@192.168.1.102>
2015-07-24 01:03:24 -0300

    Teste Consumidor Consertar atualizar

--- RestauranteUniversitarioIV/src/test/ConsumidorTest.java
+++ RestauranteUniversitarioIV/src/test/ConsumidorTest.java
@@ -16,10 +16,10 @@ import entidades.value_objects.DepartamentoVO;
 import persistencia.ConnectionFactory;
 
 public class ConsumidorTest {
-	
+
 	public void testGetInstance() 
 	{
-		
+
 		ConsumidorVO consumidorVO = new ConsumidorVO();
 		consumidorVO.setCpf("73117906118");
 		consumidorVO.setIdCurso(new Long(123));
@@ -29,30 +29,30 @@ public class ConsumidorTest {
 		consumidorVO.setSexo(SexoEnum.MASCULINO);
 		consumidorVO.setTitulo(TituloEnum.ESPECIALIZACAO);
 		consumidorVO.setAnoIngresso("2015");
-			
+
 		assertNotNull(consumidorVO);
-		
+
 	}
 	@Test
 	public void testRecuperarConsumidor() throws ConsumidorException, SQLException {
-		
+
 		ConsumidorVO consumidorVO = new ConsumidorVO();
 		consumidorVO.setMatricula("2015780000");
 		consumidorVO = Consumidor.getInstance().recuperarConsumidor(consumidorVO);
 		assertNotNull(consumidorVO);
 		assertEquals( "2015780000",consumidorVO.getMatricula());
-		
+
 	}
 	@Test
 	public void testRecuperarConsumidors() throws ConsumidorException {
-		
+
 		ConsumidorVO consumidorVO = new ConsumidorVO();
 		Consumidor.getInstance().recuperarConsumidors(consumidorVO);
 		assertNotNull(consumidorVO);
 	}
 	@Test
 	public void testAdicionarConsumidor() throws ConsumidorException, SQLException {
-		
+
 		ConsumidorVO consumidorVO = new ConsumidorVO();
 		consumidorVO.setCpf("73117906118");
 		consumidorVO.setIdCurso(new Long(123));
@@ -62,14 +62,14 @@ public class ConsumidorTest {
 		consumidorVO.setSexo(SexoEnum.MASCULINO);
 		consumidorVO.setTitulo(TituloEnum.ESPECIALIZACAO);
 		consumidorVO.setAnoIngresso("2015");
-			
+
 		assertNotNull(consumidorVO);
 		assertEquals( "José Silva", consumidorVO.getNome());
 		assertEquals( "2015780000", consumidorVO.getMatricula());
 		assertEquals( "2015", consumidorVO.getAnoIngresso());
-		
+
 		String matricula = null;
-		
+
 		try 
 		{
 			Consumidor.getInstance().manterConsumidor(consumidorVO);	
@@ -78,20 +78,46 @@ public class ConsumidorTest {
 			resultSet.next();
 			matricula = resultSet.getString("matricula");
 			assertEquals(consumidorVO.getMatricula(),matricula);
-			
+
 		} 
 		catch (ConsumidorException e)
 		{
 			throw new RuntimeException(e.getMessage(), e);
 		}
 	}
+
 	@Test
-	public void testAtualizarConsumidor() {
-		
+	public void testAtualizarConsumidor() throws ConsumidorException, SQLException {
+
+		String nome = null;
+		ConsumidorVO consumidorVO = new ConsumidorVO();
+
+		consumidorVO.setCpf("59920837407");
+		consumidorVO.setIdCurso(new Long(123));
+		consumidorVO.setNome("Maria Silva");
+		consumidorVO.setMatricula("2015780000");
+		consumidorVO.setSexo(SexoEnum.FEMININO);
+		consumidorVO.setTitulo(TituloEnum.ESPECIALIZACAO);
+		consumidorVO.setAnoIngresso("2015");
+
+		try 
+		{
+			Consumidor.getInstance().manterConsumidor(consumidorVO);
+
+			PreparedStatement prep = ConnectionFactory.getConnection().prepareStatement("select * from consumidor where matricula like '2015780000'");			
+			ResultSet resultSet = prep.executeQuery();
+			resultSet.next();
+			nome = resultSet.getString("nome");
+			
+			assertEquals( SexoEnum.FEMININO, consumidorVO.getSexo());
+			assertEquals(consumidorVO.getNome() ,nome);
+		}
+		catch (ConsumidorException e)
+		{
+			throw new RuntimeException(e.getMessage(), e);
+		}
 
 
 	}
-	
-		
-	
+
 }
